1e6b0f4dbe7f5997ca6e2ca1318caa98ea39737b
JinKyung Kim <kimradium@naver.com>
2021-12-23 15:23:35 +0900

    #1 기본 세팅

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#171A23",
+  boardColor: "#2A3556",
+  cardColor: "white",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#3F8CF2",
+  boardColor: "#DADFE9",
+  cardColor: "white",
+};
