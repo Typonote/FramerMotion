c844cb53f8644a2b53ae5174362c1ea91af9d95c
JinKyung Kim <kimradium@naver.com>
2021-12-23 22:37:45 +0900

    #7 MotionValues

--- src/index.tsx
+++ src/index.tsx
@@ -63,7 +63,7 @@ body {
   font-family: 'Source Sans Pro', sans-serif;
   color:black;
   line-height: 1.2;
-  background:linear-gradient(135deg,#2F9D27,#47C83E);
+  
 }
 a {
   text-decoration:none;
